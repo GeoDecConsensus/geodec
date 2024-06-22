00db70ee9b315c4622d7086e0e4410e09b75763c
Naman Garg <0708ng@gmail.com>
2024-06-22 15:57:42 +0530

    chore: overwrite csv file

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const markers = []
     const selectedCoordinates = []
 
-    fetch("../rundata/servers.csv")
+    fetch("./servers.csv")
         .then((response) => response.text())
         .then((csvText) => {
             Papa.parse(csvText, {
@@ -30,9 +30,24 @@ document.addEventListener("DOMContentLoaded", function () {
                                         coord.longitude === place.longitude
                                 )
                                 if (index === -1) {
+                                    place.count = 1 // Initialize count
                                     selectedCoordinates.push(place)
+                                    const input =
+                                        document.createElement("input")
+                                    input.type = "number"
+                                    input.value = place.count
+                                    input.className = "count-input"
+                                    input.onchange = function () {
+                                        place.count = parseInt(input.value)
+                                    }
+
+                                    const tooltipContent =
+                                        document.createElement("div")
+                                    tooltipContent.innerHTML = `<b>${place.name}</b>`
+                                    tooltipContent.appendChild(input)
+
                                     marker
-                                        .bindTooltip(place.name, {
+                                        .bindTooltip(tooltipContent, {
                                             permanent: true,
                                             direction: "right",
                                             className: "neon-text",
@@ -65,5 +80,17 @@ document.addEventListener("DOMContentLoaded", function () {
             link.click()
             document.body.removeChild(link)
         }
+    }
+    window.saveSelected = function () {
+        fetch("/save-coordinates", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ coordinates: selectedCoordinates }),
+        })
+            .then((response) => response.text())
+            .then((message) => alert(message))
+            .catch((error) => console.error("Error:", error))
     }
 })
